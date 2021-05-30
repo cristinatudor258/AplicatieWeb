a32d3ff0ceda2c2ffb42c0a8eeb2917115d5a5ad
Tudor Cristina <82667156+cristinatudor258@users.noreply.github.com>
2021-05-31 01:39:09 +0300

    Add files via upload

--- Controllers/HomeDataController.cs
+++ Controllers/HomeDataController.cs
@@ -33,10 +33,12 @@ namespace AplicatieWeb.Controllers
 
         internal class IntrebariClass
         {
+            public int id { get; set; }
             public string text { get; set; }
             public string responses { get; set; }
             public string raspunsCorect { get; set; }
             public int lungimeTest { get; set; }
+            public int idTest { get; set; }
             public int lungimeIntrebari { get; set; }
             public int alternareComplexitate { get; set; }
             public int durata { get; set; }
@@ -59,7 +61,7 @@ namespace AplicatieWeb.Controllers
 
         // GET api/values
         [HttpGet]
-        [Authorize(Roles="Student,Admin")]
+        [Authorize(Roles="Student,Administrator")]
         [Route("/Teste")]
         public IActionResult GetTests()
         {
@@ -76,7 +78,7 @@ namespace AplicatieWeb.Controllers
         }
 
         [HttpGet]
-        [Authorize(Roles = "Student,Admin")]
+        [Authorize(Roles = "Student,Administrator")]
         [Route("/GetTestul1")]
         public IActionResult GetTestul1()
         {
@@ -97,8 +99,8 @@ namespace AplicatieWeb.Controllers
                 var test = new List<IntrebariClass>();
 
 
-                        var intrebariUsoare = (from intrebare in _db.Intrebari where intrebare.IdTest==1 && intrebare.Dificultate == 0 && intrebare.Lungime== lungimeIntrebari select intrebare).Take(nrIntrebari);
-                        var intrebariGrele = (from intrebare in _db.Intrebari where intrebare.IdTest == 1 && intrebare.Dificultate == 1 && intrebare.Lungime == lungimeIntrebari select intrebare).Take(nrIntrebari);
+                        var intrebariUsoare = (from intrebare in _db.Intrebari where intrebare.IsDeleted!=true && intrebare.IdTest==1 && intrebare.Dificultate == 0 && intrebare.Lungime== lungimeIntrebari select intrebare).Take(nrIntrebari);
+                        var intrebariGrele = (from intrebare in _db.Intrebari where intrebare.IsDeleted != true && intrebare.IdTest == 1 && intrebare.Dificultate == 1 && intrebare.Lungime == lungimeIntrebari select intrebare).Take(nrIntrebari);
                         if (complexitateIntrebari == 0)
                         {
                     test = intrebariUsoare.Union(intrebariGrele).Select(x => new IntrebariClass
@@ -133,7 +135,7 @@ namespace AplicatieWeb.Controllers
         }
 
         [HttpGet]
-        [Authorize(Roles = "Student,Admin")]
+        [Authorize(Roles = "Student,Administrator")]
         [Route("/GetTestul2")]
         public IActionResult GetTestul2()
         {
@@ -154,8 +156,8 @@ namespace AplicatieWeb.Controllers
                 var test = new List<IntrebariClass>();
 
 
-                var intrebariUsoare = (from intrebare in _db.Intrebari where intrebare.IdTest == 2 && intrebare.Dificultate == 0 && intrebare.Lungime == lungimeIntrebari select intrebare).Take(nrIntrebari);
-                var intrebariGrele = (from intrebare in _db.Intrebari where intrebare.IdTest ==2 && intrebare.Dificultate == 1 && intrebare.Lungime == lungimeIntrebari select intrebare).Take(nrIntrebari);
+                var intrebariUsoare = (from intrebare in _db.Intrebari where intrebare.IsDeleted != true && intrebare.IdTest == 2 && intrebare.Dificultate == 0 && intrebare.Lungime == lungimeIntrebari select intrebare).Take(nrIntrebari);
+                var intrebariGrele = (from intrebare in _db.Intrebari where intrebare.IsDeleted != true && intrebare.IdTest ==2 && intrebare.Dificultate == 1 && intrebare.Lungime == lungimeIntrebari select intrebare).Take(nrIntrebari);
                 if (complexitateIntrebari == 0)
                 {
                     test = intrebariUsoare.Union(intrebariGrele).Select(x => new IntrebariClass
@@ -189,7 +191,7 @@ namespace AplicatieWeb.Controllers
             }
         }
         [HttpGet]
-        [Authorize(Roles = "Student,Admin")]
+        [Authorize(Roles = "Student,Administrator")]
         [Route("/GetTestul3")]
         public IActionResult GetTestul3()
         {
@@ -212,7 +214,7 @@ namespace AplicatieWeb.Controllers
 
                 if (lungimeIntrebari == 0)
                 {
-                    test = _db.Intrebari.Where(i=>i.IdTest==3 && i.Lungime==0).Select(x => new IntrebariClass
+                    test = _db.Intrebari.Where(i=> i.IsDeleted != true && i.IdTest==3 && i.Lungime==0 ).Select(x => new IntrebariClass
                     {
                         text = x.Intrebare,
                         responses = x.VarianteRaspuns,
@@ -224,7 +226,7 @@ namespace AplicatieWeb.Controllers
                 }
                 else
                 {
-                    test = _db.Intrebari.Where(i => i.IdTest == 3 && i.Lungime == 1).Select(x => new IntrebariClass
+                    test = _db.Intrebari.Where(i => i.IsDeleted != true && i.IdTest == 3 && i.Lungime == 1).Select(x => new IntrebariClass
                     {
                         text = x.Intrebare,
                         responses = x.VarianteRaspuns,
@@ -243,7 +245,7 @@ namespace AplicatieWeb.Controllers
             }
         }
         [HttpGet]
-        [Authorize(Roles = "Student,Admin")]
+        [Authorize(Roles = "Student,Administrator")]
         [Route("/GetTestul4")]
         public IActionResult GetTestul4()
         {
@@ -266,7 +268,7 @@ namespace AplicatieWeb.Controllers
 
                 if (lungimeIntrebari == 0)
                 {
-                    test = _db.Intrebari.Where(i => i.IdTest == 4 && i.Lungime == 0).Select(x => new IntrebariClass
+                    test = _db.Intrebari.Where(i => i.IsDeleted != true && i.IdTest == 4 && i.Lungime == 0).Select(x => new IntrebariClass
                     {
                         text = x.Intrebare,
                         responses = x.VarianteRaspuns,
@@ -278,7 +280,7 @@ namespace AplicatieWeb.Controllers
                 }
                 else
                 {
-                    test = _db.Intrebari.Where(i => i.IdTest == 4 && i.Lungime == 1).Select(x => new IntrebariClass
+                    test = _db.Intrebari.Where(i => i.IsDeleted != true && i.IdTest == 4 && i.Lungime == 1).Select(x => new IntrebariClass
                     {
                         text = x.Intrebare,
                         responses = x.VarianteRaspuns,
@@ -297,7 +299,7 @@ namespace AplicatieWeb.Controllers
             }
         }
         [HttpGet]
-        [Authorize(Roles = "Student,Admin")]
+        [Authorize(Roles = "Student,Administrator")]
         [Route("/GetEsteTerminat")]
         public IActionResult GetEsteTerminat(int idTest)
         {
@@ -325,7 +327,7 @@ namespace AplicatieWeb.Controllers
             }
         }
         [HttpPost]
-        [Authorize(Roles = "Student,Admin")]
+        [Authorize(Roles = "Student,Administrator")]
         [Route("/SalvareRezultat")]
         public IActionResult SalvareRezultat(int test, int scor,int NrIntrebari, int LungimeIntrebari, int LungimeTest = -1, int AlternareComplexitate = -1, int Durata=-1)
         {
@@ -356,7 +358,7 @@ namespace AplicatieWeb.Controllers
 
         //rapoarte
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Administrator")]
         [Route("/GetRapoarte")]
         public IActionResult GetRapoarte(int test, string tipRaport)
         {
@@ -390,6 +392,119 @@ namespace AplicatieWeb.Controllers
                 }
 
                 return Ok(rezultate);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Eroare: {ex.Message}");
+            }
+        }
+
+        //intrebari
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        [Route("/GetIntrebari")]
+        public IActionResult GetIntrebari()
+        {
+            try
+            {
+                var intrebari =  _db.Intrebari.Where(i=>i.IsDeleted!=true)
+                    .Select(r => new 
+                    {
+                        id=r.Id,
+                        text=r.Intrebare,
+                        responses=r.VarianteRaspuns,
+                        raspunsCorect=r.RaspunsCorect,
+                        idTest=r.IdTest,
+                        lungimeIntrebari=r.Lungime,
+                        alternareComplexitate=r.Dificultate
+                    }).ToList();
+
+                return Ok(intrebari);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Eroare: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        [Route("/SalvareIntrebare")]
+        public IActionResult SalvareIntrebare(int adaugareEditare, int id, string intrebare, string variante, string raspuns, 
+            string idtest, string lungime, string dificultate)
+        {
+
+            try
+            {
+                if (adaugareEditare == 0)
+                {
+                    Intrebari intrb = new Intrebari();
+                    intrb.Intrebare = intrebare;
+                    intrb.VarianteRaspuns = variante;
+                    intrb.RaspunsCorect = raspuns;
+                    intrb.IdTest = Convert.ToInt32(idtest);
+                    intrb.Lungime = Convert.ToInt32(lungime);
+                    intrb.Dificultate = Convert.ToInt32(dificultate);
+
+                    _db.Add(intrb);
+                    _db.SaveChanges();
+                    return Ok(intrb);
+                }
+                else
+                {
+                    Intrebari intrb = _db.Intrebari.Where(i => i.Id == id).FirstOrDefault();
+                    intrb.Intrebare = intrebare;
+                    intrb.VarianteRaspuns = variante;
+                    intrb.RaspunsCorect = raspuns;
+                    intrb.IdTest = Convert.ToInt32(idtest);
+                    intrb.Lungime = Convert.ToInt32(lungime);
+                    intrb.Dificultate = Convert.ToInt32(dificultate);
+
+                    _db.SaveChanges();
+                    return Ok(intrb);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Eroare: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        [Route("/StergereIntrebare")]
+        public IActionResult StergereIntrebare(int id)
+        {
+
+            try
+            {
+                Intrebari intrebare = _db.Intrebari.Where(i => i.Id == id).FirstOrDefault();
+
+                intrebare.IsDeleted = true;
+                _db.SaveChanges();
+
+                return Ok(intrebare);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Eroare: {ex.Message}");
+            }
+        }
+
+        //note
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        [Route("/GetNote")]
+        public IActionResult GetNote()
+        {
+            try
+            {
+                var note = (from rez in _db.Rezultate
+                            join stud in _db.AspNetUsers on rez.Utilizator equals stud.UserName
+                            select new { Student = stud.Nume+' '+stud.Prenume, IdTest=rez.IdTest, Nota=rez.Nota }).ToList();
+
+                return Ok(note);
             }
             catch (Exception ex)
             {
